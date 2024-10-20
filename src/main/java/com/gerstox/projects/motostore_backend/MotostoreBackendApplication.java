6ee31b4e092e4514e393676ec466f20a8e0186a9
Gerson <pinedagerson1993@gmail.com>
2024-10-20 15:14:49 -0400

    Initial commit

---
+++ src/main/java/com/gerstox/projects/motostore_backend/MotostoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gerstox.projects.motostore_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotostoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotostoreBackendApplication.class, args);
+	}
+
+}
